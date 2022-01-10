afcada5c87ea956e24b664aec1e8f55f5abfb2ee
dennisloliver <doliver9110@gmail.com>
2022-01-10 01:00:58 -0500

    updated notes

--- main.py
+++ main.py
@@ -44,10 +44,10 @@
 
 # Simple Counter
 
-my_list = [1,2,3,4,5,6,7,8,9,10]
-
-counter = 0
-for x in my_list:
-    counter = counter + x
-
-print(counter)
+# my_list = [1,2,3,4,5,6,7,8,9,10]
+#
+# counter = 0
+# for x in my_list:
+#     counter = counter + x
+#
+# print(counter)
