71fd15b781dcdd93cd988605e36eec7259ce242f
dennisloliver <doliver9110@gmail.com>
2022-01-14 01:05:20 -0500

    created a tesla prompt that checks age of drivers before giving permission to drive

--- duplicates.py
+++ duplicates.py
@@ -7,9 +7,16 @@ some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
 #If there is a duplicate pop into a new_list and move to next items
 #Print out the new_list
 
+#Creates new list for duplicates
 duplicates = []
+
+#function checks list for duplicates
 for value in some_list:
+
+#this part of the function checks to see if the count of values is greater than 1
     if some_list.count(value) > 1:
+
+#If count is greater then one it checks to see if it exist in dupllicates if it doesnt it adds it to duplicates list.
         if value not in duplicates:
             duplicates.append(value)
 print(duplicates)
